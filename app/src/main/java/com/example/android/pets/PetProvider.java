916086df1e96afd8e40615fc17af2306c6d14701
kyodgorbek <kyodgorbek@gmail.com>
2017-07-07 15:16:46 +0200

    UpdatedContentProvider

---
+++ app/src/main/java/com/example/android/pets/PetProvider.java
@@ -0,0 +1,55 @@
+package com.example.android.pets;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by yodgorbekkomilov on 7/7/17.
+ */
+
+public class PetProvider extends ContentProvider {
+
+
+    /** Tag for the log messages */
+
+    public static final String LOG_TAG = PetProvider.class.getSimpleName();
+     private PetDbHelper mDbHelper;
+
+    @Override
+    public boolean onCreate(){
+     mDbHelper = new PetDbHelper(getContext());
+       return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+}
